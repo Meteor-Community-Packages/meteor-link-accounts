9a141c2609ccc146eb26d8d853605b0de6cd9f3f
Pete Cook <pete@cookpete.com>
2015-02-17 09:43:40 +0000

    Fix instagram

--- instagram.js
+++ instagram.js
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> Add link with instagram
 if (Meteor.isClient) {
   Meteor.linkWithInstagram = function (options, callback) {
     if (!Meteor.userId()) {
       throw new Meteor.Error(402, 'Please login to an existing account before link.');
     }
     if(!Package['bozhao:accounts-instagram']) {
-<<<<<<< HEAD
-      throw new Meteor.Error(403, 'Please include bozhao:accounts-vk package')
-    }
-
-    if (!callback && typeof options === "function") {
-=======
       throw new Meteor.Error(403, 'Please include bozhao:accounts-instagram package')
     }
 
     if (! callback && typeof options === "function") {
->>>>>>> Add link with instagram
       callback = options;
       options = null;
     }
 
     var credentialRequestCompleteCallback = Accounts.oauth.linkCredentialRequestCompleteHandler(callback);
-<<<<<<< HEAD
-    Package['bozhao:accounts-instagram'].Instagram.requestCredential(options, credentialRequestCompleteCallback);
-=======
     Instagram.requestCredential(options, credentialRequestCompleteCallback);
->>>>>>> Add link with instagram
   };
 }
