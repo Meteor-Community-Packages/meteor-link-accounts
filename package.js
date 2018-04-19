1963f7c7fd67bf9bca18b91dabe4d134b0e7389e
StorytellerCZ <storyteller@freedombase.net>
2018-04-19 20:43:56 +0200

    Support for LINE :shipit:
    Support for LINE login via storyteller:accounts-line.

--- package.js
+++ package.js
@@ -41,7 +41,8 @@ Package.on_use(function (api) {
     'community-services/twitch.js',
     'community-services/venmo.js',
     'community-services/vk.js',
-    'community-services/wechat.js'
+    'community-services/wechat.js',
+    'community-services/line.js'
   ], 'client');
 });
 
