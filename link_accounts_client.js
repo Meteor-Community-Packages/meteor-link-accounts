d718b13537b8c8e6ac234c7f132a62e926df4d7a
StorytellerCZ <storyteller@freedombase.net>
2018-06-08 18:39:46 +0200

    Import client packages :v:

--- link_accounts_client.js
+++ link_accounts_client.js
@@ -1,5 +1,28 @@
 import { Accounts } from 'meteor/accounts-base';
-// TODO figure out a nice way to import services
+import 'core-services/meteor_developer.js';
+import 'core-services/facebook.js';
+import 'core-services/github.js';
+import 'core-services/google.js';
+import 'core-services/meetup.js';
+import 'core-services/twitter.js';
+import 'core-services/weibo.js';
+import 'community-services/angellist.js';
+import 'community-services/dropbox.js';
+import 'community-services/discord.js';
+import 'community-services/edmodo.js';
+import 'community-services/instagram.js';
+import 'community-services/linkedin.js';
+import 'community-services/mailru.js';
+import 'community-services/qq.js';
+import 'community-services/ok.js';
+import 'community-services/slack.js';
+import 'community-services/spotify.js';
+import 'community-services/soundcloud.js';
+import 'community-services/twitch.js';
+import 'community-services/venmo.js';
+import 'community-services/vk.js';
+import 'community-services/wechat.js';
+import 'community-services/line.js';
 
 Accounts.oauth.tryLinkAfterPopupClosed = function(credentialToken, callback) {
   const credentialSecret = OAuth._retrieveCredentialSecret(credentialToken);
--- package.js
+++ package.js
@@ -13,33 +13,4 @@ Package.onUse(function(api) {
 
   api.mainModule('link_accounts_client.js', 'client');
   api.mainModule('link_accounts_server.js', 'server');
-  api.addFiles(
-    [
-      'core-services/meteor_developer.js',
-      'core-services/facebook.js',
-      'core-services/github.js',
-      'core-services/google.js',
-      'core-services/meetup.js',
-      'core-services/twitter.js',
-      'core-services/weibo.js',
-      'community-services/angellist.js',
-      'community-services/dropbox.js',
-      'community-services/discord.js',
-      'community-services/edmodo.js',
-      'community-services/instagram.js',
-      'community-services/linkedin.js',
-      'community-services/mailru.js',
-      'community-services/qq.js',
-      'community-services/ok.js',
-      'community-services/slack.js',
-      'community-services/spotify.js',
-      'community-services/soundcloud.js',
-      'community-services/twitch.js',
-      'community-services/venmo.js',
-      'community-services/vk.js',
-      'community-services/wechat.js',
-      'community-services/line.js'
-    ],
-    'client'
-  );
 });
