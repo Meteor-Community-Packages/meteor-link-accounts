0a6d700b24d2894c6d93647b8b256499a903499c
Tom Brückner <tom@mablear.com>
2020-10-31 11:41:29 +0100

    added support for accounts-apple

---
+++ community-services/apple.js
@@ -0,0 +1,31 @@
+import { Accounts } from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor'
+
+Meteor.linkWithApple = function (options, callback, nativeCallback) {
+  if (!Meteor.userId()) {
+    throw new Meteor.Error(402, 'Please login to an existing account before link.')
+  }
+  const quaveDevPackage = Package['quave:accounts-apple'];
+  const bigowlPackage = Package['bigowl:accounts-apple'];
+  if (!quaveDevPackage && !bigowlPackage) {
+    throw new Meteor.Error(403, 'Please include quave:accounts-apple or bigowl:accounts-apple package')
+  }
+
+  if (!callback && typeof options === 'function') {
+    callback = options
+    options = null
+  }
+
+  const credentialRequestCompleteCallback = Accounts.oauth.linkCredentialRequestCompleteHandler(callback)
+  const nativeCredentialRequestCompleteCallback = Accounts.oauth.linkCredentialRequestCompleteHandler(nativeCallback || callback)
+
+  quaveDevPackage ? Package['quave:apple-oauth'].Apple.requestCredential(
+    options,
+    nativeCredentialRequestCompleteCallback,
+    credentialRequestCompleteCallback
+  ) :
+  bigowlPackage && Package['bigowl:apple-oauth'].Apple.requestCredential(
+    options,
+    credentialRequestCompleteCallback
+  )
+}
